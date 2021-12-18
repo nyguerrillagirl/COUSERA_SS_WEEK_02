70fba829ddfe5d0528b05b65d1f12c991a857151
Lorraine Figueroa <nyguerrillagirl@brainycode.com>
2021-12-17 19:21:49 -0500

    User Authentication with Passport

--- models/user.js
+++ models/user.js
@@ -1,22 +1,14 @@
-const e = require('express');
 var mongoose = require('mongoose');
-const router = require('../routes');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema( {
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
