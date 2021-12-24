15d64f159edbebf6deac221b7e280203995db152
Lorraine Figueroa <nyguerrillagirl@brainycode.com>
2021-12-24 13:19:19 -0500

    Week 3 - Second Start

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
-const Dishes = require('./models/dishes');
-const Promotions = require('./models/promotions');
-const Leaders = require('./models/leaders');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
